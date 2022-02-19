1c5a0eac0920ef1eae5af6937805d59a74aac7a9
Andrij <andrijsavickij@users.noreply.github.com>
2022-02-19 17:16:25 +0200

    struct setup v1

---
+++ app/src/main/java/com/andrj/kora/LoginRegistActivity.kt
@@ -0,0 +1,11 @@
+package com.andrj.kora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginRegistActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_regist)
+    }
+}
