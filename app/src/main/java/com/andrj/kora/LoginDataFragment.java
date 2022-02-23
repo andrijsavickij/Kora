4f61564d68f1794011128a6658bab2908c80eb83
Andrij <andrijsavickij@users.noreply.github.com>
2022-02-23 18:32:57 +0200

    logreg act try2

---
+++ app/src/main/java/com/andrj/kora/LoginDataFragment.java
@@ -0,0 +1,10 @@
+package com.andrj.kora;
+
+import androidx.fragment.app.Fragment;
+
+public class LoginDataFragment extends Fragment{
+
+    public LoginDataFragment(){
+        super(R.layout.login_data);
+    }
+}
