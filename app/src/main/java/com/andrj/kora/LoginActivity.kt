3f91079c2f1091f9446c987594e9ab8141d51c41
Andrij <andrijsavickij@users.noreply.github.com>
2022-02-23 17:58:30 +0200

    logreg act try

--- app/src/main/java/com/andrj/kora/LoginActivity.kt
+++ app/src/main/java/com/andrj/kora/LoginActivity.kt
@@ -3,9 +3,9 @@ package com.andrj.kora
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginRegistActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_regist)
+        setContentView(R.layout.activity_login)
     }
 }
